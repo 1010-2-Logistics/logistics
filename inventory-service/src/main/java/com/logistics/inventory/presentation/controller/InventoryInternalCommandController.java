d815c31de6c3a804f3ece82644143b8882927825
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-08-03 23:41:28 +0900

    chore: add endpoint 'v1'

--- inventory-service/src/main/java/com/logistics/inventory/presentation/controller/InventoryInternalCommandController.java
+++ inventory-service/src/main/java/com/logistics/inventory/presentation/controller/InventoryInternalCommandController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/internal/inventories")
+@RequestMapping("/internal/v1/inventories")
 public class InventoryInternalCommandController {
     private final InventoryCommandService inventoryCommandService;
     // TODO : 동시 재고 변경 시 어떻게 처리할 건지에 대해 구상하기 -> 동일 재고에 대한 동시 수정 충돌이 빈번하지 않을 것으로 판단하여 낙관적 락 적용 예정
@@ -38,4 +38,5 @@ public class InventoryInternalCommandController {
                 inventoryDeductionResponseDto
         ));
     }
+
 }
