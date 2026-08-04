390e7ece36a40a90c4009b55bcbdea0522d6e0e5
Seo-JS0823 <seong0093571@naver.com>
2026-08-04 14:38:59 +0900

    test: 주문시 업체 정보 조회 테스트 구현

--- company-service/src/test/java/com/logistics/company/application/service/CompanyQueryServiceTest.java
+++ company-service/src/test/java/com/logistics/company/application/service/CompanyQueryServiceTest.java
@@ -15,6 +15,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.logistics.company.application.dto.result.OrderedCompanyInfoResultDto;
+import com.logistics.company.domain.OrderedCompanyInfo;
 import com.logistics.company.domain.entity.Company;
 import com.logistics.company.domain.entity.CompanyType;
 import com.logistics.company.domain.repository.CompanyQueryRepository;
@@ -71,6 +73,109 @@ public class CompanyQueryServiceTest {
 			assertThatThrownBy(() -> companyQueryService.findByCompany(companyId))
 			.isInstanceOf(CompanyException.class)
 			.hasMessage(CompanyErrorCode.COMPANY_NOT_FOUND.getMessage());
+		}
+		
+	}
+	
+	@Nested
+	@DisplayName("주문시 업체 정보 조회")
+	class FindOrderedCompany {
+		
+		@Test
+		@DisplayName("주문시 업체 정보 조회 성공")
+		void companyInfo_ordered_success() {
+			UUID startCompanyId = UUID.randomUUID();
+			String startCompanyName = "출발 업체 이름";
+			CompanyType startCompanyType = CompanyType.PRODUCER;
+			UUID endCompanyId = UUID.randomUUID();
+			String endCompanyName = "도착 업체 이름";
+			CompanyType endCompanyType = CompanyType.RECEIVER;
+			
+			OrderedCompanyInfo companyInfo = new OrderedCompanyInfo(
+					startCompanyId,
+					startCompanyName,
+					startCompanyType,
+					endCompanyId,
+					endCompanyName,
+					endCompanyType
+			);
+			
+			given(companyQueryRepository.findOrderedCompanyInfo(startCompanyId, endCompanyId)).willReturn(Optional.of(companyInfo));
+			
+			OrderedCompanyInfoResultDto result = companyQueryService.findOrderedCompanyInfo(startCompanyId, endCompanyId);
+			
+			assertThat(result.startCompanyId()).isEqualTo(startCompanyId);
+			assertThat(result.startCompanyName()).isEqualTo(startCompanyName);
+			
+			assertThat(result.endCompanyId()).isEqualTo(endCompanyId);
+			assertThat(result.endCompanyName()).isEqualTo(endCompanyName);
+		}
+		
+		@Test
+		@DisplayName("출발 업체는 생산 업체, 도착 업체는 수령 업체여야 한다. - CASE 1")
+		void companyInfo_ordered_fail_case1() {
+			UUID startCompanyId = UUID.randomUUID();
+			String startCompanyName = "출발 업체 이름";
+			CompanyType startCompanyType = CompanyType.RECEIVER;
+			UUID endCompanyId = UUID.randomUUID();
+			String endCompanyName = "도착 업체 이름";
+			CompanyType endCompanyType = CompanyType.PRODUCER;
+			
+			assertThatThrownBy(() -> new OrderedCompanyInfo(
+					startCompanyId,
+					startCompanyName,
+					startCompanyType,
+					endCompanyId,
+					endCompanyName,
+					endCompanyType
+			))
+			.isInstanceOf(CompanyException.class)
+			.hasMessage(CompanyErrorCode.COMPANY_TYPE_FOR_ORDER.getMessage());
+		}
+		
+		@Test
+		@DisplayName("출발 업체는 생산 업체, 도착 업체는 수령 업체여야 한다. - CASE 2")
+		void companyInfo_ordered_fail_case2() {
+			UUID startCompanyId = UUID.randomUUID();
+			String startCompanyName = "출발 업체 이름";
+			CompanyType startCompanyType = CompanyType.PRODUCER;
+			UUID endCompanyId = UUID.randomUUID();
+			String endCompanyName = "도착 업체 이름";
+			CompanyType endCompanyType = CompanyType.PRODUCER;
+			
+			assertThatThrownBy(() -> new OrderedCompanyInfo(
+					startCompanyId,
+					startCompanyName,
+					startCompanyType,
+					endCompanyId,
+					endCompanyName,
+					endCompanyType
+			))
+			.isInstanceOf(CompanyException.class)
+			.hasMessage(CompanyErrorCode.COMPANY_TYPE_FOR_ORDER.getMessage());
+		}
+		
+		
+		@Test
+		@DisplayName("출발 업체는 생산 업체, 도착 업체는 수령 업체여야 한다. - CASE 3")
+		void companyInfo_ordered_fail_case3() {
+			UUID startCompanyId = UUID.randomUUID();
+			String startCompanyName = "출발 업체 이름";
+			CompanyType startCompanyType = CompanyType.RECEIVER;
+			UUID endCompanyId = UUID.randomUUID();
+			String endCompanyName = "도착 업체 이름";
+			CompanyType endCompanyType = CompanyType.RECEIVER;
+			
+			assertThatThrownBy(() -> new OrderedCompanyInfo(
+					startCompanyId,
+					startCompanyName,
+					startCompanyType,
+					endCompanyId,
+					endCompanyName,
+					endCompanyType
+					))
+			.isInstanceOf(CompanyException.class)
+			.hasMessage(CompanyErrorCode.COMPANY_TYPE_FOR_ORDER.getMessage());
 		}
 		
 	}
