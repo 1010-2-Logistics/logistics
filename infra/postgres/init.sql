8fa799760d8c3d70a62a4e42700842c242530a7a
civalwar114 <sly114@naver.com>
2026-08-04 17:40:44 +0900

    feat : 테이블 쿼리 추가

--- infra/postgres/init.sql
+++ infra/postgres/init.sql
@@ -6,3 +6,17 @@ CREATE SCHEMA IF NOT EXISTS product_service;
 CREATE SCHEMA IF NOT EXISTS inventory_service;
 CREATE SCHEMA IF NOT EXISTS order_service;
 CREATE SCHEMA IF NOT EXISTS delivery_service;
+
+CREATE TABLE IF NOT EXISTS hub_service.p_hub (
+    hub_id      UUID           DEFAULT gen_random_uuid() PRIMARY KEY,
+    hub_name    VARCHAR(100)   NOT NULL,
+    hub_address VARCHAR(255)   NOT NULL,
+    latitude    DECIMAL(10, 7) NOT NULL,
+    longitude   DECIMAL(10, 7) NOT NULL,
+    created_at  TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_by  BIGINT         NOT NULL,
+    updated_at  TIMESTAMP      NULL,
+    updated_by  BIGINT         NULL,
+    deleted_at  TIMESTAMP      NULL,
+    deleted_by  BIGINT         NULL
+    );
