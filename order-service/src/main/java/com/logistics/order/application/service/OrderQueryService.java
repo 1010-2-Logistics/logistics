2202c6569aa12aa991634418a3ffd138164d0763
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-08-04 21:46:11 +0900

    feat: add getOrder queryService

--- order-service/src/main/java/com/logistics/order/application/service/OrderQueryService.java
+++ order-service/src/main/java/com/logistics/order/application/service/OrderQueryService.java
@@ -3,7 +3,10 @@ package com.logistics.order.application.service;
 import com.logistics.order.application.dto.query.OrderSearchQuery;
 import com.logistics.order.application.dto.result.OrderDetailResult;
 import com.logistics.order.application.dto.result.OrderListResult;
+import com.logistics.order.domain.entity.Order;
 import com.logistics.order.domain.repository.OrderQueryRepository;
+import com.logistics.order.global.exception.CustomException;
+import com.logistics.order.global.exception.OrderErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,8 +20,12 @@ public class OrderQueryService {
 
     private final OrderQueryRepository orderQueryRepository;
 
+    // TODO : update ErrorCode
     public OrderDetailResult getOrder(UUID orderId) {
-        return null;
+        Order order = orderQueryRepository.findByIdAndDeletedAtIsNull(orderId)
+                .orElseThrow(() -> new CustomException(OrderErrorCode.SAMPLE_NOT_FOUND));
+
+        return OrderDetailResult.from(order);
     }
 
     public OrderListResult getOrders(OrderSearchQuery orderSearchQuery) {
