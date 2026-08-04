5c957471e2556f1cb1a637fe66eabf6b26d5c4de
Seo-JS0823 <seong0093571@naver.com>
2026-08-04 22:53:39 +0900

    fix: RequestParam -> PathVariable CompanyQueryController companyGetOne Method

--- company-service/src/main/java/com/logistics/company/presentation/controller/CompanyQueryController.java
+++ company-service/src/main/java/com/logistics/company/presentation/controller/CompanyQueryController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +28,7 @@ public class CompanyQueryController {
 	private final CompanyQueryService companyQueryService;
 	
 	@GetMapping("/{companyId}")
-	public ApiResponse<CompanyInfoResponseDto> companyGetOne(@RequestParam("companyId") UUID companyId) {
+	public ApiResponse<CompanyInfoResponseDto> companyGetOne(@PathVariable("companyId") UUID companyId) {
 		CompanyInfoResponseDto response = CompanyInfoResponseDto.from(companyQueryService.findByCompany(companyId));
 		
 		return ApiResponse.success(
