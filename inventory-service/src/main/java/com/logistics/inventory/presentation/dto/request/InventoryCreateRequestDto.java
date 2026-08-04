3dec78177b3ae5e45ead6294e101ca313946fac0
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-08-04 12:48:16 +0900

    feat: 재고 생성 요청값 검증 추가

--- inventory-service/src/main/java/com/logistics/inventory/presentation/dto/request/InventoryCreateRequestDto.java
+++ inventory-service/src/main/java/com/logistics/inventory/presentation/dto/request/InventoryCreateRequestDto.java
@@ -1,10 +1,19 @@
 package com.logistics.inventory.presentation.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
 import java.util.UUID;
 
 public record InventoryCreateRequestDto(
+        @NotNull
         UUID productId,
+
+        @NotNull
         UUID hubId,
+
+        @NotNull
+        @PositiveOrZero // 숫자가 0 이상인지 검사하는 Jakarta Validation 애너테이션
         Integer stock
 ) {
 }
