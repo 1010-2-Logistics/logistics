b69f8955683e52821bebc34fbb9a5f08b795b303
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-08-04 20:16:07 +0900

    test:주문 생성하고 저장 성공

--- order-service/src/test/java/com/logistics/order/application/facade/OrderFacadeTest.java
+++ order-service/src/test/java/com/logistics/order/application/facade/OrderFacadeTest.java
@@ -5,6 +5,9 @@ import com.logistics.order.infrastructure.feign.client.CompanyClient;
 import com.logistics.order.infrastructure.feign.client.DeliveryClient;
 import com.logistics.order.infrastructure.feign.client.InventoryClient;
 import com.logistics.order.infrastructure.feign.client.ProductClient;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -30,4 +33,32 @@ class OrderFacadeTest {
 
     @InjectMocks
     private OrderFacade orderFacade;
+
+    @Nested
+    @DisplayName("주문 생성")
+    class order_create{
+        @Test
+        @DisplayName("성공")
+        void order_create_success(){
+
+        }
+
+        @Test
+        @DisplayName("업체 조회 실패 시 이후 호출 중단")
+        void order_create_company(){
+
+        }
+
+        @Test
+        @DisplayName("재고 차감 실패 시 배송과 주문 생성 중단")
+        void order_create_inventory(){
+
+        }
+
+        @Test
+        @DisplayName("배송 생성 실패 시 주문 저장 중단")
+        void order_create_fail_save(){
+
+        }
+    }
 }
---
+++ order-service/src/test/java/com/logistics/order/application/service/OrderCommandServiceTest.java
@@ -0,0 +1,81 @@
+package com.logistics.order.application.service;
+
+import com.logistics.order.application.dto.command.OrderCreateCommand;
+import com.logistics.order.application.dto.result.OrderCreateResult;
+import com.logistics.order.application.port.EventPublisher;
+import com.logistics.order.domain.entity.Order;
+import com.logistics.order.domain.entity.OrderStatus;
+import com.logistics.order.domain.repository.OrderCommandRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class OrderCommandServiceTest {
+
+    @Mock
+    private OrderCommandRepository orderCommandRepository;
+
+    @Mock
+    private EventPublisher eventPublisher;
+
+    @InjectMocks
+    private OrderCommandService orderCommandService;
+
+    @Nested
+    @DisplayName("주문 생성")
+    class order_create {
+        @Test
+        @DisplayName("주문 생성하고 저장 성공")
+        void order_create_success() {
+            UUID orderId = UUID.randomUUID();
+            UUID deliveryId = UUID.randomUUID();
+            UUID endCompanyId = UUID.randomUUID();
+            UUID productId = UUID.randomUUID();
+
+            OrderCreateCommand orderCreateCommand = new OrderCreateCommand(
+                    endCompanyId,
+                    productId,
+                    10,
+                    "8월 6일 오전까지 납품"
+            );
+
+            given(orderCommandRepository.save(any(Order.class))).willAnswer(invocation -> invocation.getArgument(0));
+
+            OrderCreateResult result = orderCommandService.createOrder(
+                    orderCreateCommand,
+                    orderId,
+                    deliveryId
+            );
+
+            ArgumentCaptor<Order> orderCaptor = ArgumentCaptor.forClass(Order.class);
+
+            verify(orderCommandRepository).save(orderCaptor.capture());
+
+            Order savedOrder = orderCaptor.getValue();
+
+            assertThat(savedOrder.getOrderId()).isEqualTo(orderId);
+            assertThat(savedOrder.getDeliveryId()).isEqualTo(deliveryId);
+            assertThat(savedOrder.getEndCompanyId()).isEqualTo(endCompanyId);
+            assertThat(savedOrder.getProductId()).isEqualTo(productId);
+            assertThat(savedOrder.getQuantity()).isEqualTo(10);
+            assertThat(savedOrder.getRequest()).isEqualTo("8월 6일 오전까지 납품");
+            assertThat(savedOrder.getStatus()).isEqualTo(OrderStatus.CREATED);
+
+            assertThat(result.orderId()).isEqualTo(orderId);
+        }
+    }
+}
