a443f59d4eef6e3e7fb45afd333917f96e4220e2
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-08-05 10:53:12 +0900

    feat: controller update

--- inventory-service/src/main/java/com/logistics/inventory/application/dto/command/InventoryUpdateCommand.java
+++ inventory-service/src/main/java/com/logistics/inventory/application/dto/command/InventoryUpdateCommand.java
@@ -1,6 +1,15 @@
 package com.logistics.inventory.application.dto.command;
 
-public record InventoryUpdateCommand(
+import com.logistics.inventory.presentation.dto.request.InventoryUpdateRequestDto;
 
+public record InventoryUpdateCommand(
+        Integer stock
 ) {
+    public static InventoryUpdateCommand toCommand(
+            InventoryUpdateRequestDto inventoryUpdateRequestDto
+    ) {
+        return new InventoryUpdateCommand(
+                inventoryUpdateRequestDto.stock()
+        );
+    }
 }
--- inventory-service/src/main/java/com/logistics/inventory/presentation/dto/response/InventoryCreateResponseDto.java
+++ inventory-service/src/main/java/com/logistics/inventory/presentation/dto/response/InventoryCreateResponseDto.java
@@ -1,8 +1,17 @@
 package com.logistics.inventory.presentation.dto.response;
 
+import com.logistics.inventory.application.dto.result.InventoryCreateResult;
+
 import java.util.UUID;
 
 public record InventoryCreateResponseDto(
         UUID inventoryId
 ) {
+    public static InventoryCreateResponseDto from(
+            InventoryCreateResult inventoryCreateResultDto
+    ) {
+        return new InventoryCreateResponseDto(
+                inventoryCreateResultDto.inventoryId()
+        );
+    }
 }
