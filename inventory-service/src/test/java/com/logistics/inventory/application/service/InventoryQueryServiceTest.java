423f0f5558d55f3791f304bfcbab31832e78b5a3
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-08-05 18:07:36 +0900

    test: 재고 조회 서비스 테스트 구조 추가

--- inventory-service/src/test/java/com/logistics/inventory/application/service/InventoryQueryServiceTest.java
+++ inventory-service/src/test/java/com/logistics/inventory/application/service/InventoryQueryServiceTest.java
@@ -1,12 +1,39 @@
 package com.logistics.inventory.application.service;
 
+import com.logistics.inventory.domain.repository.InventoryQueryRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @ExtendWith(MockitoExtension.class)
 class InventoryQueryServiceTest {
+    UUID inventoryId = UUID.randomUUID();
+    UUID productId = UUID.randomUUID();
+    UUID hubId = UUID.randomUUID();
+
+    @Mock
+    private InventoryQueryRepository inventoryQueryRepository;
+
+    @InjectMocks
+    private InventoryQueryService inventoryQueryService;
+
+    @Nested
+    @DisplayName("재고 단건 조회")
+    class inventory_detail {
+    }
+
+    @Nested
+    @DisplayName("재고 전체 조회")
+    class inventory_search {
+
+    }
 
 }
