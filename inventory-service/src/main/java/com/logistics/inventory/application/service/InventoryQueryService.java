0f8fd47f7f3e2a8934bcb271ec62207389af0dd3
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-08-05 10:14:40 +0900

    feat: 단건조회

--- inventory-service/src/main/java/com/logistics/inventory/application/service/InventoryQueryService.java
+++ inventory-service/src/main/java/com/logistics/inventory/application/service/InventoryQueryService.java
@@ -3,10 +3,11 @@ package com.logistics.inventory.application.service;
 import com.logistics.inventory.application.dto.query.SearchInventoryQuery;
 import com.logistics.inventory.application.dto.result.InventoryGetOneResult;
 import com.logistics.inventory.application.dto.result.InventoryListResult;
-import com.logistics.inventory.application.dto.result.InventorySummaryResult;
+import com.logistics.inventory.domain.entity.Inventory;
 import com.logistics.inventory.domain.repository.InventoryQueryRepository;
+import com.logistics.inventory.global.exception.CustomException;
+import com.logistics.inventory.global.exception.InventoryErrorCode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,7 +23,11 @@ public class InventoryQueryService {
     public InventoryGetOneResult getInventory(
             UUID inventoryId
     ) {
-        return null;
+        Inventory inventory = inventoryQueryRepository
+                .findByInventoryIdAndDeletedAtIsNull(inventoryId)
+                .orElseThrow(() -> new CustomException(InventoryErrorCode.INVENTORY_NOT_FOUND));
+
+        return InventoryGetOneResult.from(inventory);
     }
 
     public InventoryListResult searchInventory(
--- inventory-service/src/main/java/com/logistics/inventory/presentation/controller/InventoryQueryController.java
+++ inventory-service/src/main/java/com/logistics/inventory/presentation/controller/InventoryQueryController.java
@@ -3,16 +3,11 @@ package com.logistics.inventory.presentation.controller;
 import com.logistics.inventory.application.dto.query.SearchInventoryQuery;
 import com.logistics.inventory.application.dto.result.InventoryGetOneResult;
 import com.logistics.inventory.application.dto.result.InventoryListResult;
-import com.logistics.inventory.application.dto.result.InventorySummaryResult;
 import com.logistics.inventory.application.service.InventoryQueryService;
 import com.logistics.inventory.global.response.ApiResponse;
-import com.logistics.inventory.global.response.PageResponse;
 import com.logistics.inventory.presentation.dto.response.InventoryGetOneResponseDto;
 import com.logistics.inventory.presentation.dto.response.InventoryListResponseDto;
-import com.logistics.inventory.presentation.dto.response.InventorySummaryResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -41,7 +36,7 @@ public class InventoryQueryController {
     }
 
     @GetMapping
-    public ResponseEntity<ApiResponse<PageResponse<InventorySummaryResponseDto>>> searchInventory(
+    public ResponseEntity<ApiResponse<InventoryListResponseDto>> searchInventory(
             @RequestParam(required = false) UUID productId,
             @RequestParam(required = false) UUID hubId,
             @RequestParam(defaultValue = "createdAt") String sort,
@@ -63,7 +58,7 @@ public class InventoryQueryController {
         return ResponseEntity.ok(ApiResponse.success(
                 200,
                 "재고 목록 조회 성공",
-                PageResponse.of(inventoryListResponseDto)
+                inventoryListResponseDto
         ));
     }
 }
