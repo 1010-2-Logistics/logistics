433dae7c4b962fc270f97f2d8d09c63560531cec
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-08-06 22:05:38 +0900

    feat: facade 연결

--- slack-service/src/main/java/com/logistics/slack/application/facade/SlackFacade.java
+++ slack-service/src/main/java/com/logistics/slack/application/facade/SlackFacade.java
@@ -2,6 +2,7 @@ package com.logistics.slack.application.facade;
 
 import com.logistics.slack.application.dto.command.SlackCreateCommand;
 import com.logistics.slack.application.dto.result.SlackCreateResult;
+import com.logistics.slack.application.service.SlackCommandService;
 import com.logistics.slack.presentation.dto.response.SlackRetryResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,14 +13,23 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class SlackFacade {
     // userClient 조립
+    private final SlackCommandService slackCommandService;
+
     public SlackCreateResult createSlack(
             SlackCreateCommand slackCreateCommand
     ) {
         return null;
     }
 
-    public SlackRetryResponseDto retrySlackMessage(UUID slackMessageId) {
-        return null;
+    public SlackCreateResult retrySlackMessage(
+            Long slackMessageId,
+            SlackCreateCommand slackCreateCommand
+
+    ) {
+        return slackCommandService.createSlack(
+                slackMessageId,
+                slackCreateCommand
+        );
     }
 
 }
