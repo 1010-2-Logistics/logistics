bfc7b8b9b44215a487f5c93f179a44a9261dec76
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-08-07 19:45:32 +0900

    test: 조회 성공 테스트

--- slack-service/src/test/java/com/logistics/slack/application/service/SlackQueryServiceTest.java
+++ slack-service/src/test/java/com/logistics/slack/application/service/SlackQueryServiceTest.java
@@ -1,6 +1,10 @@
 package com.logistics.slack.application.service;
 
+import com.logistics.slack.application.dto.query.SlackSearchQuery;
+import com.logistics.slack.application.dto.result.SlackDetailResult;
+import com.logistics.slack.application.dto.result.SlackListResult;
 import com.logistics.slack.domain.entity.Slack;
+import com.logistics.slack.domain.entity.SlackStatus;
 import com.logistics.slack.domain.repository.SlackQueryRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -9,9 +13,21 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import javax.swing.text.html.Option;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -50,7 +66,14 @@ class SlackQueryServiceTest {
         @Test
         @DisplayName("슬랙 메시지 상세 조회 성공")
         void slack_detail_success() {
+            Slack slack = createSlack();
 
+            given(slackQueryRepository.findByIdAndDeletedAtIsNull(slackMessageId)).willReturn(Optional.of(slack));
+
+            SlackDetailResult slackDetailResult = slackQueryService.getSlack(slackMessageId);
+
+            assertThat(slackDetailResult).isNotNull();
+            verify(slackQueryRepository).findByIdAndDeletedAtIsNull(slackMessageId);
         }
 
         @Test
@@ -66,7 +89,47 @@ class SlackQueryServiceTest {
         @Test
         @DisplayName("슬랙 메시지 전체 조회 성공")
         void slack_lise_empty() {
+            LocalDateTime createdFrom = LocalDateTime.of(2030, 8, 1, 0, 0);
+            LocalDateTime createdTo = LocalDateTime.of(2030, 8, 2, 0, 0);
+            Pageable pageable = PageRequest.of(0, 10);
+            SlackSearchQuery slackSearchQuery = SlackSearchQuery.of(
+                    SlackStatus.SUCCESS,
+                    senderId,
+                    receiverId,
+                    referenceId,
+                    createdFrom,
+                    createdTo,
+                    pageable
+            );
+            Slack slack = createSlack();
+            Page<Slack> slackPage = new PageImpl<>(
+                    List.of(slack),
+                    pageable,
+                    1
+            );
 
+
+            given(slackQueryRepository.search(
+                    SlackStatus.SUCCESS,
+                    senderId,
+                    receiverId,
+                    referenceId,
+                    createdFrom,
+                    createdTo,
+                    pageable
+            )).willReturn(slackPage);
+            Page<SlackListResult> result = slackQueryService.getSlacks(slackSearchQuery);
+            assertThat(result).hasSize(1);
+
+            verify(slackQueryRepository).search(
+                    SlackStatus.SUCCESS,
+                    senderId,
+                    receiverId,
+                    referenceId,
+                    createdFrom,
+                    createdTo,
+                    pageable
+            );
         }
     }
 
