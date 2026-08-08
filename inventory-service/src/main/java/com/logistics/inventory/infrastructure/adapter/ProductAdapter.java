ef439bc2561483aec1c391acf1450f3e657a7c1e
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-08-08 19:06:33 +0900

    feat: ProductAdapter 추가

--- inventory-service/src/main/java/com/logistics/inventory/infrastructure/adapter/ProductAdapter.java
+++ inventory-service/src/main/java/com/logistics/inventory/infrastructure/adapter/ProductAdapter.java
@@ -1,4 +1,26 @@
 package com.logistics.inventory.infrastructure.adapter;
 
-public class ProductAdapter {
+import com.logistics.inventory.application.dto.internal.response.ProductExistsResponseDto;
+import com.logistics.inventory.application.port.ProductPort;
+import com.logistics.inventory.infrastructure.feign.client.ProductClient;
+import com.logistics.inventory.infrastructure.feign.response.ProductValidationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class ProductAdapter implements ProductPort {
+    private final ProductClient productClient;
+
+    @Override
+    public ProductExistsResponseDto getProduct(UUID productId) {
+        ProductValidationResponse productValidationResponse = productClient.getProduct(productId);
+
+        return new ProductExistsResponseDto(
+                productValidationResponse.productId(),
+                productValidationResponse.exists()
+        );
+    }
 }
