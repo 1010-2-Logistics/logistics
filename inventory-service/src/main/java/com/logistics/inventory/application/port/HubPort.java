6cb39d35c8b2c3b6ad5ac3d7eacf20d86162f6e0
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-08-08 19:01:13 +0900

    chore: HubPort & ProductPort 추가

--- inventory-service/src/main/java/com/logistics/inventory/application/port/HubPort.java
+++ inventory-service/src/main/java/com/logistics/inventory/application/port/HubPort.java
@@ -1,4 +1,9 @@
 package com.logistics.inventory.application.port;
 
-public class HubPort {
+import com.logistics.inventory.application.dto.internal.response.HubExistsResponseDto;
+
+import java.util.UUID;
+
+public interface HubPort {
+    HubExistsResponseDto getHub(UUID hubId);
 }
--- inventory-service/src/main/java/com/logistics/inventory/application/port/ProductPort.java
+++ inventory-service/src/main/java/com/logistics/inventory/application/port/ProductPort.java
@@ -1,4 +1,9 @@
 package com.logistics.inventory.application.port;
 
-public class ProductPort {
+import com.logistics.inventory.application.dto.internal.response.ProductExistsResponseDto;
+
+import java.util.UUID;
+
+public interface ProductPort {
+    ProductExistsResponseDto getProduct(UUID productId);
 }
