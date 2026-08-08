77c4bd090d422fa2d917e6b218cc844fa846d31a
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-08-08 19:04:27 +0900

    feat: HubClientAdapter 추가

--- inventory-service/src/main/java/com/logistics/inventory/infrastructure/adapter/HubClientAdapter.java
+++ inventory-service/src/main/java/com/logistics/inventory/infrastructure/adapter/HubClientAdapter.java
@@ -1,4 +1,27 @@
 package com.logistics.inventory.infrastructure.adapter;
 
-public class HubClientAdapter {
+import com.logistics.inventory.application.dto.internal.response.HubExistsResponseDto;
+import com.logistics.inventory.application.port.HubPort;
+import com.logistics.inventory.infrastructure.feign.client.HubClient;
+import com.logistics.inventory.infrastructure.feign.response.HubValidationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class HubClientAdapter implements HubPort {
+    private final HubClient hubClient;
+
+
+    @Override
+    public HubExistsResponseDto getHub(UUID hubId) {
+        HubValidationResponse hubValidationResponse = hubClient.getHub(hubId);
+
+        return new HubExistsResponseDto(
+                hubValidationResponse.hubId(),
+                hubValidationResponse.exists()
+        );
+    }
 }
--- inventory-service/src/main/java/com/logistics/inventory/infrastructure/feign/client/HubClient.java
+++ inventory-service/src/main/java/com/logistics/inventory/infrastructure/feign/client/HubClient.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 // name은 Eureka에 등록된 대상 서비스의 spring.application.name과 일치해야 합니다.
-@FeignClient(name = "hub-service", configuration = FeignConfig.class)
+@FeignClient(
+        name = "hub-service",
+        configuration = FeignConfig.class)
 public interface HubClient {
 
     @GetMapping("/internal/v1/hubs/{hubId}")
