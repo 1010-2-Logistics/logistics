8d9abebf4a0d0267fa756e215005c60d7f116a8b
Seo-JS0823 <seong0093571@naver.com>
2026-08-09 01:09:40 +0900

    fix: Value에 변수값 미작성한 것 수정

--- ai-service/src/main/java/com/logistics/ai/infrastructure/messaging/OrderCreatedEventListener.java
+++ ai-service/src/main/java/com/logistics/ai/infrastructure/messaging/OrderCreatedEventListener.java
@@ -13,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class OrderCreatedEventListener {
 
-	@RabbitListener(queues = "${}")
+	@RabbitListener(queues = "${rabbitmq.order-created.queue}")
 	public void handle(OrderCreatedEvent event) {
 		
 	}
