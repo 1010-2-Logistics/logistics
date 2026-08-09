405ef110cfac489af4e091c831f6da0ff173f664
civalwar114 <sly114@naver.com>
2026-08-09 14:24:42 +0900

    fix 리뷰 반영

--- hubRoute-service/src/main/java/com/logistics/hubRoute/application/service/HubRouteCommandService.java
+++ hubRoute-service/src/main/java/com/logistics/hubRoute/application/service/HubRouteCommandService.java
@@ -124,7 +124,6 @@ public class HubRouteCommandService {
     public void deleteHubRoutesByHubId(UUID hubId, Long deletedBy) {
         List<HubRoute> routes = hubRouteCommandRepository.findAllByStartHubIdOrEndHubIdAndDeletedAtIsNull(hubId, hubId);
 
-        System.out.println(">>> 조회된 연관 허브 경로 개수: " + routes.size());
 
         for (HubRoute hubRoute : routes) {
             hubRoute.markDeleted(deletedBy);
