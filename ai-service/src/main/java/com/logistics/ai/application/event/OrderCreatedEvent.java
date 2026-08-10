7eb4f478958a646dbe964a0655e00b12a8a58c3c
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-08-11 00:18:32 +0900

    chore: ai-service 변경사항 제외

--- ai-service/src/main/java/com/logistics/ai/application/event/OrderCreatedEvent.java
+++ ai-service/src/main/java/com/logistics/ai/application/event/OrderCreatedEvent.java
@@ -1,16 +1,10 @@
 package com.logistics.ai.application.event;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record OrderCreatedEvent(
-        UUID orderId,
-        UUID deliveryId,
-        UUID productId,
-        Integer quantity,
-        String request,
-        String receiverName,
-        String receiverSlackId,
-        LocalDateTime createdAt
+		UUID orderId,
+		UUID deliveryId
 ) {
+
 }
