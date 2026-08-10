6b8b62e63f514968724439dd9ab9bbbcae0cafd0
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-08-11 08:13:23 +0900

    feat: security 복제

---
+++ inventory-service/src/main/java/com/logistics/inventory/domain/entity/Role.java
@@ -0,0 +1,27 @@
+package com.logistics.inventory.domain.entity;
+
+public enum Role {
+    MASTER,
+
+    HUB_MANAGER,
+
+    HUB_DELIVERY_MANAGER,
+
+    COMPANY_MANAGER,
+
+    COMPANY_DELIVERY_MANAGER
+
+    ;
+
+    public boolean isMaster() {
+        return this == MASTER;
+    }
+
+    public boolean isHubManager() {
+        return this == HUB_MANAGER;
+    }
+
+    public boolean isCompanyManager() {
+        return this == COMPANY_MANAGER;
+    }
+}
