ea5108ea9684974dae0eac88526d8abd9e9f7b85
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-08-10 12:39:24 +0900

    feat: add OrderUpdateOrchestrator

--- order-service/src/main/java/com/logistics/order/application/saga/OrderUpdateOrchestrator.java
+++ order-service/src/main/java/com/logistics/order/application/saga/OrderUpdateOrchestrator.java
@@ -1,4 +1,18 @@
 package com.logistics.order.application.saga;
 
+
+import com.logistics.order.application.service.OrderCommandService;
+import com.logistics.order.infrastructure.feign.client.InventoryClient;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class OrderUpdateOrchestrator {
+    private final OrderCommandService orderCommandService;
+    private final InventoryClient inventoryClient;
+
+
 }
