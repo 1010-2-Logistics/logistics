d4829aaeaa18047723919fe1b10befc41e267d2e
Seo-JS0823 <seong0093571@naver.com>
2026-08-11 13:13:16 +0900

    feat: 인가 작업 완료

--- company-service/src/main/java/com/logistics/company/global/exception/CompanyErrorCode.java
+++ company-service/src/main/java/com/logistics/company/global/exception/CompanyErrorCode.java
@@ -17,7 +17,11 @@ public enum CompanyErrorCode implements ErrorCode {
 	
 	COMPANY_USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 회원입니다."),
 	
-	COMPANY_DUPLICATE_HUB_NAME(HttpStatus.CONFLICT, "소속 허브에 동일한 업체명이 존재합니다.")
+	COMPANY_DUPLICATE_HUB_NAME(HttpStatus.CONFLICT, "소속 허브에 동일한 업체명이 존재합니다."),
+	
+	COMPANY_NOT_SELF_HUB(HttpStatus.FORBIDDEN, "소속 허브가 아닙니다."),
+	
+	COMPANY_NOT_SELF_COMPANY(HttpStatus.FORBIDDEN, "소속 업체가 아닙니다."),
 	;
 	
   private final HttpStatus httpStatus;
