032d33e10a10333e0846a46ca5da0ca6b2674229
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-08-11 14:59:01 +0900

    feat: sprign security 추가

---
+++ slack-service/src/main/java/com/logistics/slack/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.logistics.slack.infrastructure.config;
+
+import com.logistics.slack.domain.entity.Role;
+import com.logistics.slack.infrastructure.security.filter.HeaderAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+
+                        // PATCH /api/v1/companies/manager/fix
+                        .requestMatchers(HttpMethod.PATCH, "/api/v1/companies/update/manager/fix").hasAnyRole(
+                                Role.MASTER.name(), Role.HUB_MANAGER.name()
+                        )
+
+                        // DELETED /api/v1/companies/{companyId}
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/companies/{companyId}").hasAnyRole(
+                                Role.MASTER.name(), Role.HUB_MANAGER.name(), Role.COMPANY_MANAGER.name()
+                        )
+
+                        // PATCH /api/v1/companies/{companyId}
+                        .requestMatchers(HttpMethod.PATCH, "/api/v1/companies/{companyId}").hasAnyRole(
+                                Role.MASTER.name(), Role.HUB_MANAGER.name(), Role.COMPANY_MANAGER.name()
+                        )
+
+                        // POST /api/v1/companies
+                        .requestMatchers(HttpMethod.POST, "/api/v1/companies").hasAnyRole(
+                                Role.MASTER.name(), Role.HUB_MANAGER.name()
+                        )
+
+                        .requestMatchers(HttpMethod.GET,
+                                "/api/v1/companies/{companyId}",
+                                "/api/v1/companies"
+                        ).permitAll()
+
+                        // 내부 API 권한 검증은 어떻게?
+                        .requestMatchers(
+                                "/internal/v1/companies",
+                                "/internal/v1/companies/{companyId}/exists"
+                        ).permitAll()
+
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new HeaderAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+
+        ;
+        return http.build();
+    }
+}
