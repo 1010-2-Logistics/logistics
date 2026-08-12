34f721c314be8f10cf8838275acb69da0dd417fe
Seo-JS0823 <seong0093571@naver.com>
2026-08-12 09:21:56 +0900

    fix: 테스트 코드 수정ㅇ

--- company-service/src/test/java/com/logistics/company/application/facade/CompanyFacadeTest.java
+++ company-service/src/test/java/com/logistics/company/application/facade/CompanyFacadeTest.java
@@ -89,9 +89,9 @@ public class CompanyFacadeTest {
 					true
 			);
 			given(userPort.companyManagerRoleUpdateRequest(UserRoleUpdateRequestDto.from(
-					companyCreateCommand.companyManagerId(),
-					company
-			))).willReturn(userRoleUpdate);
+					company),
+					companyCreateCommand.companyManagerId()
+			)).willReturn(userRoleUpdate);
 			
 			company.updateCompanyManager(companyManagerId);
 			company.updateStatus(CompanyStatus.ACTIVE);
