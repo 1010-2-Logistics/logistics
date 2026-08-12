35913aa806c3ac0988a90283ff661da7973d1c43
summmmmmmin <jsoomin01@naver.com>
2026-08-12 20:51:25 +0900

    fix : 충돌 해결..

--- company-service/src/main/java/com/logistics/company/global/exception/CompanyErrorCode.java
+++ company-service/src/main/java/com/logistics/company/global/exception/CompanyErrorCode.java
@@ -25,7 +25,7 @@ public enum CompanyErrorCode implements ErrorCode {
 	
 	COMPANY_NOT_SELF_COMPANY(HttpStatus.FORBIDDEN, "소속 업체가 아닙니다."),
 
-	COMPANY_HUB_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 허브입니다."),
+	//COMPANY_HUB_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 허브입니다."),
 	
 	COMPANY_NAME_UPDATE_PRODUCT_FAIL(HttpStatus.CONFLICT, "업체와 관련된 상품의 업데이트에 실패했습니다."),
 	
--- hub-service/src/main/java/com/logistics/hub/presentation/dto/dto/response/HubSummaryResponseDto.java
+++ hub-service/src/main/java/com/logistics/hub/presentation/dto/dto/response/HubSummaryResponseDto.java
@@ -3,7 +3,7 @@ package com.logistics.hub.presentation.dto.dto.response;
 import com.logistics.hub.domain.entity.Hub;
 import java.util.UUID;
 
-public record HubSummaryResponseDto(UUID sampleId, String name) {
+public record HubSummaryResponseDto(UUID hubId, String name) {
 
     public static HubSummaryResponseDto from(Hub hub) {
         return new HubSummaryResponseDto(hub.getHubId(), hub.getHubName());
